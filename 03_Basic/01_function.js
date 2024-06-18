8a5fad035dde225893bfc61edc8657e1406f6000
Aakanksha77 <akanksharajbhar77@gmail.com>
2024-06-18 23:36:47 +0530

    lesrning about function

---
+++ 03_Basic/01_function.js
@@ -0,0 +1,11 @@
+function myfunction () {
+    console.log("hello");
+}
+
+myfunction    //refernces of the fuction
+myfunction(); // this is the execution of the function 
+
+// "function" is a keyword 
+// "myfunction" is a name of function with ()
+// By using {} we action the fuction
+// to call the function we call the name of the function with ().
