52e5155a2e0c288a68a794d34526b47503ab6176
Aakanksha77 <akanksharajbhar77@gmail.com>
2024-06-20 20:27:21 +0530

    Object's in function

--- 03_Basic/01_function.js
+++ 03_Basic/01_function.js
@@ -22,8 +22,48 @@ let sum = (addTwoNumber(3,4));
 
 console.log("Result: ",sum);
 
-function loginMessage(username){
+function loginMessage(username = "user"){
+    if (!username) {
+        return
+    };
     return `${username} Welcome to helping Desk`
 }
 
-console.log(loginMessage("Aakanksha"));
+ console.log(loginMessage("Aakanksha"));
+
+
+// 
+
+function calculateCartPrice(val1, val2, ...num1) { //this ... is a reset operator 
+
+    return  num1;
+}
+
+console.log(calculateCartPrice(200, 300, 800, 500, 400, 100, 1000));
+
+
+// Object in function
+// 1 way
+
+const user = {
+    name : "aakanksha",
+    price : 998
+};
+
+function handleObject(anyObject) {
+    console.log(`User name is ${anyObject.name} and price is ${anyObject.price}`)
+}
+
+handleObject(user);
+
+
+//  2 way
+
+function userBill(billObject) {
+    console.log (`User name is ${billObject.userinfo} and probuct price is ${billObject.prices}`)   
+}
+
+userBill({
+    userinfo : "Mansi",
+    prices : 499
+})
