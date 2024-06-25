f44f9cf47efd23a8d4905b5dfd7db00c4ad65869
Aakanksha77 <akanksharajbhar77@gmail.com>
2024-06-25 22:25:37 +0530

    completed with iife

--- 03_Basic/04_iife.js
+++ 03_Basic/04_iife.js
@@ -6,6 +6,12 @@ function database() {
 
 database();
 
-(function database1() {
+(function database1() {          //this is named iife
     console.log(`This is database`);
-} )()
+} )();
+
+
+((user) => {
+    console.log(`heello Aakanksha . I am ${user}`)
+}) ("Rashami");
+
