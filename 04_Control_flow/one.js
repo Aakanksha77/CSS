5b1547641d475d45451fc3a75125524942b331db
Aakanksha77 <akanksharajbhar77@gmail.com>
2024-06-27 22:36:33 +0530

    new code line added

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,16 @@
+//If
+
+if (true) {
+    
+};
+
+const temp = 45;
+
+if (temp < 50) {
+    console.log("the temperature is less then 50")
+} else {
+    console.log("Then Temperture is greater then 50")
+}
+
+ 
+
