4e53b73ebedceb1cbefe5c7f3975359d40a386d4
Aakanksha77 <akanksharajbhar77@gmail.com>
2024-06-28 17:48:13 +0530

    switch part cover

---
+++ 04_Control_flow/switch.js
@@ -0,0 +1,47 @@
+// Following is the representation of switch statement
+
+const key = "value"
+switch (key) {
+    case "value":
+        
+        break;
+
+    default:
+        break;
+}   
+
+const weak = 4;
+
+switch (weak) {
+    case 1:
+        console.log("Monday")
+        break;
+
+    case 2:
+        console.log("Tuesday")
+        break;
+
+    case 3:
+        console.log("Wednesday")
+        break;
+
+    case 4:
+        console.log("Thursday")
+        break;
+
+    case 5:
+        console.log("Friday")
+        break;
+
+    case 6:
+        console.log("Saturday")
+        break;
+
+    case 7:
+        console.log("Sunday")
+        break;
+
+    default:
+        console.log("None match")
+        break;
+}
