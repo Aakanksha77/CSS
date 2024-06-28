f167ec639d9d8fb183c251d7bb7bfab90568b0ff
Aakanksha77 <akanksharajbhar77@gmail.com>
2024-06-28 17:16:34 +0530

    if statement

--- 04_Control_flow/one.js
+++ 04_Control_flow/one.js
@@ -12,5 +12,38 @@ if (temp < 50) {
     console.log("Then Temperture is greater then 50")
 }
 
- 
+//  Nesting when we have to check multipl condition
+
+const bankbalance = 1000;
+
+if (bankbalance < 500) {
+    console.log("Balance is greater then 500");
+}else if (bankbalance < 700){
+    console.log("Balance is greater then 700");
+}else if (bankbalance < 900){
+    console.log("Balance greater then 900")
+}else{
+    console.log("Balance is less then 1200")
+}
+
+
+// Let see some real life example
+
+const userLoggedIn = true;
+const debitCard = false;
+
+if (userLoggedIn && debitCard) {
+    console.log("You are allow to logIn");
+}else{
+    console.log("Not allowed")
+};
+
+const loggedByGoogle = true;
+const loggedByEmail = false;
+
+if (loggedByGoogle || loggedByEmail) {
+    console.log("Logged In");
+}else{
+    console.log("Pls Retry again");
+};
 
