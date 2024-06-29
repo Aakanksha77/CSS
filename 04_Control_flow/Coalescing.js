fca9aee7d289a1ae28d923b1255c72123b7152e1
Aakanksha77 <akanksharajbhar77@gmail.com>
2024-06-29 21:44:31 +0530

    Coalesting and Terniary Operator added

---
+++ 04_Control_flow/Coalescing.js
@@ -0,0 +1,16 @@
+// Nullish Coalescing Operator  (???): null undefined
+
+let val;
+// val = 5 ?? 10
+val = null ?? 10
+
+
+console.log(val)
+
+// Terniary Operator
+
+// condition ? true : false 
+
+const icePrice = 100;
+
+icePrice <= 80 ? console.log("less then 80") : console.log("More then 80")
