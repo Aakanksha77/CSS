271835ec68ade3aa7a8a131acc61bb8d8a79f71e
Aakanksha77 <akanksharajbhar77@gmail.com>
2024-07-09 22:42:27 +0530

    do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,15 @@
+// While loop
+
+let index = 1;
+while (index <= 10) {
+    // console.log(`Value of index i : ${index}`)
+    index = index+3
+}
+
+// Do While loop:
+
+let score = 2;
+do {
+    console.log(`${score}`)
+    score+=2 
+} while (score <= 10);
