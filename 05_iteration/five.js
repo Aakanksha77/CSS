9f1c78216e13f21f7507b0db201af10211af5f37
Aakanksha77 <akanksharajbhar77@gmail.com>
2024-07-14 15:38:01 +0530

    ForEach Loop added

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -1,1 +1,45 @@
 const coding = ['js','ry', 'cpp','py'];
+
+coding.forEach( function (params) {
+    console.log(params);
+})  // as it is a call back function it doesn't have name in it .The value In the paraenties () will act as a paramenter because there are inside the array 
+
+// On other version of the above :
+
+//By using arrow function
+
+coding.forEach( (iteam) => {
+    console.log(iteam);
+} )
+
+//One other way:
+
+function printMe(values) {
+    console.log(`I am print Me: ${values}`)
+}
+
+coding.forEach(printMe) //Here printMe is reference 
+
+// In the following we are access the object value stored in array 
+const myObject = [
+    {
+        Laungage: "Python",
+        Extention: "py"
+    },
+    {
+        Laungage: "Java",
+        Extention: "Java"
+    },
+    {
+        Laungage: "C#",
+        Extention: "cpy"
+    },
+    {
+        Laungage: "Kotlin",
+        Extention: "kl"
+    }
+];
+
+myObject.forEach( (iteam)=>{
+    console.log(iteam.Extention);
+})
